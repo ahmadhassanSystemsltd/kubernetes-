8494d7ccfbc6426f590bf5680fc64d9a51035dcf
ahmadhassanSystemsltd <109286114+ahmadhassanSystemsltd@users.noreply.github.com>
2022-12-07 15:04:31 +0500

    Update master.sh

--- master.sh
+++ master.sh
@@ -1,113 +1,34 @@
 #!/bin/bash
 
-yum -y upgrade
+echo "Updating Yum...."
+sudo yum -y update
 
-echo "Add Kubernetes repository"
-sudo tee /etc/yum.repos.d/kubernetes.repo<<EOF
+echo "Installing Kubelet, Kubeadm and Kubectl......"
+cat <<EOF | sudo tee /etc/yum.repos.d/kubernetes.repo
 [kubernetes]
 name=Kubernetes
-baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
+baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-\$basearch
 enabled=1
 gpgcheck=1
-repo_gpgcheck=0
+repo_gpgcheck=1
 gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+exclude=kubelet kubeadm kubectl
 EOF
 
 sudo yum -y install epel-release vim git curl wget kubelet-1.23.2 kubeadm-1.23.2-0 kubectl-1.23.2 --disableexcludes=kubernetes
 
-echo "Disable SELinux and Swap"
+echo "Your Kubeadm version is......"
+sudo kubeadm version
+
+echo "Checking the current status of selinux"
+sudo sestatus
+
+echo "Disabling SE Linux and Swap for You"
 sudo setenforce 0
-sudo sed -i 's/^SELINUX=.*/SELINUX=permissive/g' /etc/selinux/config
+sudo sed -i 's/^SELINUX=.*/SELINUX=disabled/g' /etc/selinux/config
+
 sudo sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
 sudo swapoff -a
 
-echo "Configure sysctl"
-sudo modprobe overlay
-sudo modprobe br_netfilter
-
-sudo tee /etc/sysctl.d/kubernetes.conf<<EOF
-net.bridge.bridge-nf-call-ip6tables = 1
-net.bridge.bridge-nf-call-iptables = 1
-net.ipv4.ip_forward = 1
-EOF
-
-sudo sysctl --system
-
-echo "install container runtime"
-# Install packages
-sudo yum install -y yum-utils device-mapper-persistent-data lvm2
-sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-sudo yum install docker-ce docker-ce-cli containerd.io
-
-# Create required directories
-sudo mkdir /etc/docker
-sudo mkdir -p /etc/systemd/system/docker.service.d
-
-# Create daemon json config file
-sudo tee /etc/docker/daemon.json <<EOF
-{
-  "exec-opts": ["native.cgroupdriver=systemd"],
-  "log-driver": "json-file",
-  "log-opts": {
-    "max-size": "100m"
-  },
-  "storage-driver": "overlay2",
-  "storage-opts": [
-    "overlay2.override_kernel_check=true"
-  ]
-}
-EOF
-
-# Start and enable Services
-sudo systemctl daemon-reload 
-sudo systemctl restart docker
-sudo systemctl enable docker
-
-echo "disable firewalld"
-sudo systemctl disable --now firewalld
-
-sudo firewall-cmd --add-port={6443,2379-2380,10250,10257,10259}/tcp --permanent
-sudo firewall-cmd --reload
-
-lsmod | grep br_netfilter
-sudo systemctl enable kubelet
-suo systemctl start kubelet
-sudo kubeadm config images pull
-
-echo "Kubeadm Intialzing Advertising the Public IP on This Master Node"
-sudo kubeadm init --pod-network-cidr=10.244.0.0/16 --upload-certs --control-plane-endpoint=$YOUR_IP
-
-echo "Creating Folders and giveing permissions to run Kubectl Commands"
-sudo mkdir -p $HOME/.kube
-sudo cp /etc/kubernetes/admin.conf $HOME/
-sudo chown $(id -u):$(id -g) $HOME/admin.conf
-export KUBECONFIG=$HOME/admin.conf
-
-#Fix the Error – The connection to the server localhost:8080 was refused
-#export KUBECONFIG=/etc/kubernetes/admin.conf
-
-echo "Bootstrapping Kubectl Commands"
-echo 'export KUBECONFIG=$HOME/admin.conf' >> $HOME/.bashrc
-
-echo "Checking Status of Nodes"
-kubectl get nodes
-
-echo "Installing Flannel Cli"
-kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
-echo "Checking if Flannel and Core DNS is Installed"
-kubectl get pods -n kube-system
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+echo "Restaring your system to accept selinux disabled"
+sudo init 6
